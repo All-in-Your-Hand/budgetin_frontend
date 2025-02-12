430c03fc52f131e386fac5c9052b6f10653c34e9
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2025-02-13 01:08:37 +0300

    Update mock_data for testing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:budgetin_frontend/src/features/authentication/presentation/providers/auth_provider.dart';
+import 'package:budgetin_frontend/src/features/transaction/presentation/pages/transaction_page.dart';
 import 'package:flutter/material.dart';
-import 'src/features/authentication/presentation/auth_injections.dart';
-import 'src/features/authentication/presentation/pages/sign_up_page.dart';
+import 'src/features/authentication/auth_injections.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const SignUpPage(),
+      home: const TransactionPage(),
     );
   }
 }
--- lib/src/features/authentication/auth_injections.dart
+++ lib/src/features/authentication/auth_injections.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import '../../../core/utils/constant/network_constants.dart';
-import '../data/data_sources/remote/auth_remote_data_source.dart';
-import '../data/repositories/auth_repository_impl.dart';
-import '../domain/repositories/auth_repository.dart';
-import '../presentation/providers/auth_provider.dart';
+import '../../core/utils/constant/network_constants.dart';
+import 'data/data_sources/remote/auth_remote_data_source.dart';
+import 'data/repositories/auth_repository_impl.dart';
+import 'domain/repositories/auth_repository.dart';
+import 'presentation/providers/auth_provider.dart';
 
 void setupAuthInjections() {
   final getIt = GetIt.instance;
